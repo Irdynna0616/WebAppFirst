71eda2167768b360b291343c4301b44242bfc279
Irdynna0616 <nashairdynna@gmail.com>
2022-10-03 22:53:51 +0800

    Add project files.

---
+++ WebAppFirst/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppFirst.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult LogIn(string username, string pwd)
+        {
+            if(username != null && pwd != null)
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+           
+            return View();
+        }
+      
+    }
+}
---
+++ WebAppFirst/Controllers/RegistrationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppFirst.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult SignUp(string name, string mail, string pwd)
+        {
+            if (name != null && mail != null && pwd != null )
+            {
+                return RedirectToAction("Account", "LogIn");
+            }
+
+            List<SelectListItem> li1 = new List<SelectListItem>();
+            li1.Add(new SelectListItem { Text = "Please Choose", Value = "Please Choose" });
+            li1.Add(new SelectListItem { Text = "Please Choose", Value = "Please Choose " });
+            ViewData["Gender"] = li1;
+
+            return View();
+        }
+        public ActionResult visitorRegister()
+        {
+            {
+                List<SelectListItem> li2 = new List<SelectListItem>();
+                li2.Add(new SelectListItem { Text = "Please Choose", Value = "Please Choose" });
+                li2.Add(new SelectListItem { Text = "Please Choose", Value = "Please Choose " });
+                ViewData["RoadNumber"] = li2;
+            }
+            {
+                List<SelectListItem> li3 = new List<SelectListItem>();
+                li3.Add(new SelectListItem { Text = "Please Choose", Value = "Please Choose" });
+                li3.Add(new SelectListItem { Text = "Please Choose", Value = "Please Choose " });
+                ViewData["Reason"] = li3;
+
+            }
+
+           
+
+            return View();
+        }
+    }
+}
